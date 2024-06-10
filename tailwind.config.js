3fac59ad8d3f9e966558c78d162b21f45d0c3934
Abno <abhinavmaurya605@gmail.com>
2024-06-10 21:42:48 +0530

    tailwind file configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        kumbh: ["Kumbh Sans", "sans-serif"],
+      },
+      colors: {
+        "primary": "hsl(26, 100%, 55%)",
+        "secondary": "hsl(25, 100%, 94%)",
+        "dark-blue": "hsl(220, 13%, 13%)",
+        "light-grayish-blue": "hsl(223, 64%, 98%)",
+        "white": "hsl(0, 0%, 100%)",
+        "black": "hsl(0, 0%, 0%)",
+      },
+    },
   },
   plugins: [],
 }
