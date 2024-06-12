ec3621f1f63f85c483da8480ce0dfab9e00a7a1f
Abno <abhinavmaurya605@gmail.com>
2024-06-12 21:37:48 +0530

    navbar done for mobile

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,30 @@
 <template>
-    <header>
-        <nav class="max-container border border-red-400"></nav>
-    </header>
+  <header>
+    <nav class="p-4 md:max-container">
+      <div class="flex justify-between items-center">
+        <div class="flex justify-between items-center gap-3">
+          <div class="mt-3">
+            <span class="material-symbols-outlined text-gray-500 font-bold">
+              menu
+            </span>
+          </div>
+          <div>
+            <h1 class="font-bold text-3xl">sneakers</h1>
+          </div>
+        </div>
+        <div class="flex justify-between items-center gap-6">
+          <div class="mt-2">
+            <span class="material-symbols-outlined"> shopping_cart </span>
+          </div>
+          <div>
+            <a href="#">
+              <img src="/images/image-avatar.png" alt="" class="w-8" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
 </template>
-<script setup>
-</script>
+<script setup></script>
 <style scoped></style>
